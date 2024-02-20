f816fc3a8bdd93c8149e3b59910e325a837444cd
RyanJWallis <159253042+RyanJWallis@users.noreply.github.com>
2024-02-20 12:26:29 +0000

    Add files via upload

---
+++ 0_Z-Score Heatmaps/Models - Z Score Heat Map.R
@@ -0,0 +1,47 @@
+library("gplots")
+library("RColorBrewer")
+
+
+# Upload your file 
+all <- read.csv("Models Z-Score Vs Control.csv", stringsAsFactors = F)
+
+
+### Can adjust model selection here ### 
+samp2 <- all[,-1]
+rownames(samp2) <- all[,1]
+all<-samp2
+  
+
+
+#Convert to data matrix 
+all_matrix <- t((as.matrix(all)))
+
+# Create heat map
+breaks <- unique(c(seq(-10,-1.96,length=100),seq(-1.96,1.96,length=100), seq(1.96,10,length=100)))
+my_palette <- colorRampPalette(c("Blue","white","white","red"))(length(breaks)-1)
+heatmap.2(all_matrix,
+          Rowv = T,
+          Colv = F,
+          col=my_palette,
+          breaks=breaks,
+          density.info="none",
+          trace="none",
+          #main = "199 compounds; Length or Prev",
+          dendrogram=c("both"), 
+          symm=F,symkey=F,symbreaks=T,
+          labRow= F,
+          labCol= colnames(all_matrix),
+          cexCol = 0.7,
+          cexRow = 0.8,
+          margins = c(5,5),
+          key.title = "1" , key.xlab="Z Score", 
+          #rowsep =c(0,4,12,19,31,37,43),
+          #colsep =c(0,233),
+          sepcolor = c("black"),sepwidth = c(0.05, 0.05),
+          #ColSideColors=condition_colors, scale="none",
+          distfun = function(x) dist(x, method = "euclidean"),
+          hclust=function(x) hclust(x,method="ward.D2"))
+
+
+
+
